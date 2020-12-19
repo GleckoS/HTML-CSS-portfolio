3fdd4fd09c72d6dbf4738327c2e9421f96621a42
Glecko <shevabogdan16@gmail.com>
2020-12-19 21:59:36 +0100

    more media

--- porten_clock_1/js/index.js
+++ porten_clock_1/js/index.js
@@ -1,9 +1,20 @@
 const openClose = (id) => {
-    let el = document.getElementsByClassName("content")
+    let el = document.getElementsByClassName("footer-content")
     if (el[id].classList.contains('active')) {
         el[id].classList.remove('active')
     } else {
-        el[id].clientWidth
         el[id].classList.add('active');
+    }
+}
+
+const mobileNav = () => {
+    let el = document.getElementById("nav-search__mobile")
+    let el2 = document.getElementById("nav-list")
+    if (el.classList.contains('active') && el2.classList.contains('active')) {
+        el2.classList.remove('active')
+        el.classList.remove('active')
+    } else {
+        el2.classList.add('active')
+        el.classList.add('active');
     }
 }
